e4ecf0ac50bb9c02df2d1c30bab9660a0d1fdf73
Alongkot <alongkot317@gmail.com>
2023-04-24 18:45:33 +0700

    test broadcasting

--- server/server.js
+++ server/server.js
@@ -10,9 +10,28 @@ const io = new Server({
 io.on("connection", (socket) => {
   console.log("connected", socket.id);
   socket.emit("welcome",{ status:200, message:`Welcome socket:${socket.id} to Server`});
+
   socket.on("greeting", (...args) => {
     console.log(args);
   });
+
+  socket.on('login', (username, password, callback) =>{
+    console.log('login:',username, password);
+    callback({
+      status:200,
+      message: "login"
+    })
+  });
+
+  socket.on('broadcast', (message, callback) =>{
+    console.log('broadcast from:',socket.id, 'message:',message);
+    io.emit(`broadcast-all`,{ status:200, message:message, sender:socket.id});
+    callback({
+      status:200,
+      message: 'send broadcast successfuly'
+    })
+  });
+
 });
 
 console.log('start server');
