f1961ad05175628e33b2f18f35173dd9594827a1
Alongkot <alongkot317@gmail.com>
2023-04-28 13:14:21 +0700

    clear unused chat.js,group.js

--- server/controller/chat.js
+++ server/controller/chat.js
@@ -1,11 +1,10 @@
-// const {createMessage, GetMessages} = require('../db/db');
+const {createMessage, GetMessages} = require('../db/db');
 const {getUserFromId} = require('./user');
 const {getGroupFromId} = require('./group');
 
-//not use
-let messages = Array();
 
-exports.sendMessage = (senderId, receiverId, message, is_direct) =>{
+// emit
+exports.sendMessage = (io,senderId, receiverId, message, is_direct) =>{
     if(is_direct){
         const target = getUserFromId(receiverId);
         if(target){
@@ -15,7 +14,6 @@ exports.sendMessage = (senderId, receiverId, message, is_direct) =>{
             io.to(target.socketId).emit("directMessage",mes);
             // createMessage(senderId,receiverId,parseInt(is_direct, 10),message);
             mes.receiverId = receiverId;
-            messages.push(mes);
             return true;
         }
     }else{
@@ -26,16 +24,11 @@ exports.sendMessage = (senderId, receiverId, message, is_direct) =>{
             let mes = {senderId:senderId,groupId:targetId,message:message,timestamp:timestamp};
             io.in('g-' + targetId).emit("groupMessage",mes);
             mes.receiverId = receiverId;
-            messages.push(mes);
             return true;
         }
     }
 };
 
 exports.getMessageHistory = (is_direct, receiverId, senderId) =>{
-    if(is_direct){
-        return messages;
-    }else{
-        return messages;
-    }
+    
 };
--- server/controller/group.js
+++ server/controller/group.js
@@ -1,30 +1,19 @@
-// const {GetRooms, GetUserInRooms} = require('../db/db');
+const {GetGroups, GetUserInGroup} = require('../db/db');
 
-//not use
-let groups = new Map();
-let index = 99999;
 
+//emit
 exports.createGroup = (socket, groupName, creatorId, bgColor) =>{
-    groups.set(index,{groupName:groupName, creatorId:creatorId, bgColor:bgColor, users:[creatorId,]});
-    return {groupId:index, groupName:groupName, creatorId:creatorId, bgColor:bgColor};
+   
 };
 
 exports.joinGroup = (userId, groupId) =>{
-    groupId = parseInt(groupId, 10);
-    if(groups.has(groupId)){
-        let temp = groups.get(groupId);
-        temp.users.push(userId);
-        return true;
-    }
+    
 };
 
 exports.getGroups = () =>{
-    return groups;
+    
 };
 
-exports.getGroupFromId = (groupId) =>{
-    groupId = parseInt(groupId, 10);
-    if(groups.has(groupId)){
-        return groupId.toString();
-    }
+exports.getUserInGroup = (groupId) =>{
+    
 }
